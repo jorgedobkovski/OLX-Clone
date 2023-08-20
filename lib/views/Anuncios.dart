371e50f53ab37ef5c8b249adaedd4e64d9ce04d4
Jorge Dobkovski <47338154+jorgedobkovski@users.noreply.github.com>
2023-08-20 00:12:22 -0400

    feat: dropdowns dos filtros
    criando os dropdowns dos filtros de categoria e estado na tela anuncios

--- lib/views/Anuncios.dart
+++ lib/views/Anuncios.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../util/Configuracoes.dart';
+
+
 class Anuncios extends StatefulWidget {
   const Anuncios({Key? key}) : super(key: key);
 
@@ -11,6 +14,10 @@ class Anuncios extends StatefulWidget {
 class _AnunciosState extends State<Anuncios> {
 
   List<String> itensMenu = [""];
+  String? _itemSelecionadoEstado;
+  String? _itemSelecionadoCategoria;
+  List<DropdownMenuItem<String>> _listaDropEstados = List.empty(growable: true);
+  List<DropdownMenuItem<String>> _listaDropCategorias = List.empty(growable: true);
 
   _escolhaMenuItem(String itemEscolhido){
 
@@ -50,10 +57,18 @@ class _AnunciosState extends State<Anuncios> {
 
   }
 
+  _carregarItensDropdown(){
+
+    _listaDropCategorias = Configuracoes.getCategorias();
+    _listaDropEstados = Configuracoes.getEstados();
+
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _carregarItensDropdown();
     _verificarUsuarioLogado();
   }
 
@@ -76,6 +91,58 @@ class _AnunciosState extends State<Anuncios> {
               },
           )
         ],
+      ),
+      body: Container(
+        child: Column(children: <Widget>[
+          Row(children: <Widget>[
+            Expanded(
+              child: DropdownButtonHideUnderline(
+                child: Center(
+                  child: DropdownButton(
+                    iconEnabledColor: Color(0xff9c27b0),
+                    value: _itemSelecionadoEstado,
+                    items: _listaDropEstados,
+                    style: TextStyle(
+                        fontSize: 22,
+                      color: Colors.black,
+                    ),
+                    onChanged: (estado){
+                      setState((){
+                        _itemSelecionadoEstado = estado;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.grey[200],
+              width: 2,
+              height: 60,
+            ),
+            Expanded(
+              child: DropdownButtonHideUnderline(
+                child: Center(
+                  child: DropdownButton(
+                    iconEnabledColor: Color(0xff9c27b0),
+                    value: _itemSelecionadoCategoria,
+                    items: _listaDropCategorias,
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.black,
+                    ),
+                    onChanged: (categoria){
+                      setState((){
+                        _itemSelecionadoCategoria = categoria;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+          )
+        ],),
       ),
     );
   }
